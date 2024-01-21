963c33996be2fefe23813f75b3a8cff8915f5c61
PronayBormon <pronaykumarbormon@gmail.com>
2024-01-21 12:20:47 +0600

    html file

--- navbar.php
+++ navbar.php
@@ -23,47 +23,47 @@
 <!-- navbar start here  -->
 <nav class="navbar navbar-expand-lg  ">
   <div class="container">
-    <a class="navbar-brand" href="index.php"><img src="images/logo.gif" alt=""></a>
+    <a class="navbar-brand" href="index.html"><img src="images/logo.gif" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="wedo.php" role="button">
+          <a class="nav-link dropdown-toggle" href="wedo.html" role="button">
             What We Do
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="wedo.php#core">Core Service Areas</a></li>
-            <li><a class="dropdown-item" href="wedo.php#consult_model">Our Consulting Model</a></li>
-            <li><a class="dropdown-item" href="wedo.php#serve">industries We Serve</a></li>
+            <li><a class="dropdown-item" href="wedo.html#core">Core Service Areas</a></li>
+            <li><a class="dropdown-item" href="wedo.html#consult_model">Our Consulting Model</a></li>
+            <li><a class="dropdown-item" href="wedo.html#serve">industries We Serve</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="story.php" role="button">
+          <a class="nav-link dropdown-toggle" href="story.html" role="button">
             What We Are
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="story.php#story">Our Story</a></li>
-            <li><a class="dropdown-item" href="team.php">Our Team</a></li>
+            <li><a class="dropdown-item" href="story.html#story">Our Story</a></li>
+            <li><a class="dropdown-item" href="team.html">Our Team</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="talent.php" role="button">
+          <a class="nav-link dropdown-toggle" href="talent.html" role="button">
             Talent
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="talent.php#why">Why astute360corp? </a></li>
-            <li><a class="dropdown-item" href="findtalent.php">Find a telent</a></li>
-            <li><a class="dropdown-item" href="becometalent.php">Become a telent</a></li>
+            <li><a class="dropdown-item" href="talent.html#why">Why astute360corp? </a></li>
+            <li><a class="dropdown-item" href="findtalent.html">Find a telent</a></li>
+            <li><a class="dropdown-item" href="becometalent.html">Become a telent</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="contact.php" role="button">
+          <a class="nav-link dropdown-toggle" href="contact.html" role="button">
             Contact
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="contact.php">Our Locations</a></li>
+            <li><a class="dropdown-item" href="contact.html">Our Locations</a></li>
           </ul>
         </li>
 
